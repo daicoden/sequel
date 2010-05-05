bbcc50ecb626dbdc8718ce2552ee98e8465e1c69
☁ daicoden <daicoden@copypastel.com>
2010-05-05 16:39:15 -0600

    Last commit did not pass tests.  We are now moving along while honoring the sequel method of testing, at least I think.

--- lib/sequel/plugins/proc_error_handling.rb
+++ lib/sequel/plugins/proc_error_handling.rb
@@ -1,10 +1,22 @@
 module Sequel
   module Plugins
+    # Methodologies
+    #
+    # Practically it is best if you return a modified model.
+    # If you want to return false or some other variable then you
+    # will have to put in checks in the control code.  The whole
+    # point of the proc error handling is to provide correction code
+    # to allow the control code to function normaly.  Anything else
+    # and the code should enter the exceptional code.
+    #
+    # #new is enforced this way since it can only return an instance
+    # of itself.  In the future #create, #update, #set may be modified
+    # so that only a model, :retry, :raise, or nil may be returned
     module ProcErrorHandling
       def self.apply(model)
         model.class_eval do
           class << self
-            alias orig_create create
+            alias peh_orig_create create
           end
         end
       end
@@ -17,10 +29,14 @@ module Sequel
 
       module ClassMethods
         def create(values = {}, error_proc = nil, &block)
-          orig_create(values,&block)
+          peh_orig_create(values,&block)
         rescue 
           raise $! unless error_proc
-          result = error_proc.call(self.class,values) 
+          error_proc = [error_proc] unless error_proc.is_a? Array
+          result = error_proc.each do |ep|
+            val = ep.call(self,values) 
+            break val if val            
+          end
           raise $! if result == :raise or !result
           retry if result == :retry
           result
